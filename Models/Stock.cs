0bd88bd1d006ec5ce40283aa820a46e3c6a76b3f
Sehej Wahla <sehejwahla@gmail.com>
2024-10-13 00:06:00 +1100

    first commit

---
+++ Models/Stock.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myApiProject.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string String { get; set; }
+    }
+}
